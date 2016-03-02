5e1d059f654b91a6bd4d02e147858458a0e43a58
qwattash <mzz.lrd@gmail.com>
2016-03-02 17:12:55 +0100

    Add simple AXI4-lite slave interface
    Currently the PoC creates two slaves at fixed addresses, the read operation works however the rprot signal is ignored. The write operation works, the write response is work in progress

---
+++ peripheral/Axi4Lite.scala
@@ -0,0 +1,191 @@
+package cam.cl.chiselipxact;
+
+import Chisel._;
+
+object AXI4LiteBusWidth {
+  val AXI32 = 32
+  val AXI64 = 64
+}
+
+class PeripheralSlaveIf extends Bundle {
+    val in = Decoupled(new Bundle {
+      val wdata = UInt(INPUT, AXI4LiteBusWidth.AXI32)
+    }).flip()
+
+    val out = Decoupled(new Bundle {
+      val rdata = UInt(OUTPUT, AXI4LiteBusWidth.AXI32)
+    })
+}
+
+class PeripheralMasterIf extends Bundle {
+  val in = Decoupled(new Bundle {
+    val wdata = UInt(OUTPUT, AXI4LiteBusWidth.AXI32)
+  })
+
+  val out = Decoupled(new Bundle {
+    val rdata = UInt(INPUT, AXI4LiteBusWidth.AXI32)
+  }).flip()
+}
+
+class Axi4LiteSlaveIf extends Bundle {
+    // write address channel
+    val awchannel = Decoupled(new Bundle {
+      val awaddr = UInt(INPUT, AXI4LiteBusWidth.AXI32)
+      val awprot = Bits(INPUT, 3)
+    }).flip()
+
+    // write data channel
+    val wchannel = Decoupled(new Bundle {
+      val wdata = UInt(INPUT, AXI4LiteBusWidth.AXI32)
+      val wstrb = UInt(INPUT, 4)
+    }).flip()
+
+    // write response channel
+    val bchannel = Decoupled(new Bundle {
+      val bresp = UInt(OUTPUT, 2)
+    })
+
+    // read address channel
+    val archannel = Decoupled(new Bundle {
+      val araddr = UInt(INPUT, AXI4LiteBusWidth.AXI32)
+      val arprot = Bits(INPUT, 3)
+    }).flip()
+
+    // read data channel
+    val rchannel = Decoupled(new Bundle {
+      val rdata = UInt(OUTPUT, AXI4LiteBusWidth.AXI32)
+      val rresp = Bits(OUTPUT, 2)
+    })
+}
+
+class Peripheral(i: Int) extends Module {
+  val io = new PeripheralSlaveIf
+
+  val out = Reg(init = UInt(i, AXI4LiteBusWidth.AXI32))
+  io.in.ready := Bool(true)
+  io.out.valid := Bool(true)
+  io.out.bits.rdata := out
+
+  // when (io.out.ready) {
+  // }
+  when (io.in.valid) {
+    out := io.in.bits.wdata
+  }
+}
+
+class AXI4LiteSlave extends Module {
+  val io = new Bundle {
+
+    val axi = new Axi4LiteSlaveIf
+    // attached peripheral
+    val slaveA = new PeripheralMasterIf
+    val slaveB = new PeripheralMasterIf
+
+  }
+
+  /* handle read address channel for multiplexing */
+
+  // cached address
+  val addr = Reg(init = UInt(0, AXI4LiteBusWidth.AXI32))
+  // read ok after address has been set
+  val rvalid = Reg(init = Bool(false))
+
+  io.axi.archannel.ready := ! rvalid
+
+  // set address when it becomes valid
+  when (io.axi.archannel.valid && io.axi.archannel.ready) { 
+    addr := io.axi.archannel.bits.araddr
+    rvalid := Bool(true)
+  }
+
+  // reset address after read
+  when (io.axi.rchannel.valid && io.axi.rchannel.ready) {
+    rvalid := Bool(false)
+  }
+
+  /* handle write address channel for multiplexing */
+
+  // cached address
+  val waddr = Reg(init = UInt(0, AXI4LiteBusWidth.AXI32))
+  // write ok after address has been set
+  val wvalid = Reg(init = Bool(false))
+
+  io.axi.awchannel.ready := ! wvalid
+
+  // set address when it becomes valid
+  when (io.axi.awchannel.valid && io.axi.awchannel.ready) { 
+    addr := io.axi.awchannel.bits.awaddr
+    wvalid := Bool(true)
+  }
+
+  // reset address after read
+  when (io.axi.wchannel.valid && io.axi.wchannel.ready) {
+    wvalid := Bool(false)
+  }
+
+  /* handle the write response */
+
+  val wresp_valid = Reg(init = Bool(false))
+  val wresp = Reg(init = UInt(0, 2))
+  io.axi.bchannel.valid := wresp_valid
+  io.axi.bchannel.bits.bresp := wresp
+
+  when (io.axi.wchannel.valid && io.axi.wchannel.ready) {
+    wresp := Bool(true)
+  }
+
+  when (io.axi.bchannel.valid && io.axi.bchannel.ready) {
+    wresp := Bool(false)
+  }
+
+  /* forward read channel */
+
+  when (addr === UInt(0x00)) {
+    io.axi.rchannel.bits.rdata := io.slaveA.out.bits.rdata
+    io.slaveA.out.ready := io.axi.rchannel.ready
+    io.axi.rchannel.valid := io.slaveA.out.valid && rvalid
+
+    io.slaveB.out.ready := Bool(false)
+
+  } .elsewhen (addr === UInt(0x04)) {
+    io.axi.rchannel.bits.rdata := io.slaveB.out.bits.rdata
+    io.slaveB.out.ready := io.axi.rchannel.ready
+    io.axi.rchannel.valid := io.slaveB.out.valid && rvalid
+
+    io.slaveA.out.ready := Bool(false)
+
+  } .otherwise {
+    io.axi.rchannel.bits.rdata := UInt(0x00)
+    io.slaveA.out.ready := Bool(false)
+    io.slaveB.out.ready := Bool(false)
+    io.axi.rchannel.valid := Bool(false)
+  }
+
+  /* forward write channel */
+
+  when (addr === UInt(0x00)) {
+    io.slaveA.in.bits.wdata := io.axi.wchannel.bits.wdata
+    io.slaveA.in.valid := io.axi.wchannel.valid
+    io.axi.wchannel.ready := io.slaveA.in.ready && wvalid && !wresp
+
+    io.slaveB.in.valid := Bool(false)
+    io.slaveB.in.bits.wdata := UInt(0x00)
+
+  } .elsewhen (addr === UInt(0x04)) {
+    io.slaveB.in.bits.wdata := io.axi.wchannel.bits.wdata
+    io.slaveB.in.valid := io.axi.wchannel.valid
+    io.axi.wchannel.ready := io.slaveB.in.ready && wvalid && !wresp
+
+    io.slaveA.in.valid := Bool(false)
+    io.slaveA.in.bits.wdata := UInt(0x00)
+
+  } .otherwise {
+    io.slaveA.in.bits.wdata := UInt(0x00)
+    io.slaveB.in.bits.wdata := UInt(0x00)
+    io.slaveA.in.valid := Bool(false)
+    io.slaveB.in.valid := Bool(false)
+    io.axi.wchannel.ready := Bool(false)
+  }
+
+
+}
---
+++ peripheral/Testbench.scala
@@ -0,0 +1,110 @@
+
+package cam.cl.chiselipxact;
+
+import Chisel._;
+
+class Axi4LiteTest(c: AXI4LiteSlave) extends Tester(c) {
+
+  println("Test start")
+  println("Test read")
+  poke(c.io.axi.archannel.valid, true)
+  poke(c.io.axi.archannel.bits.araddr, 0x00)
+  poke(c.io.axi.archannel.bits.arprot, 0x00)
+  step(1)
+  expect(peek(c.io.axi.rchannel.bits.rdata) == 0xdeadbeef, "simple read")
+  // while (peek(c.io.archannel.ready) == 0)
+  //   step(1)
+  println("Test end")
+
+}
+
+class IPBlockTest(c: IPBlock) extends Tester(c) {
+
+  def read (addr: Int, good: Int) = {
+    poke(c.io.archannel.valid, true)
+    poke(c.io.archannel.bits.araddr, addr)
+    poke(c.io.archannel.bits.arprot, 0x00)
+    // complete addr hs
+    while (peek(c.io.archannel.ready) == 0)
+      step(1)
+    step(1)
+    poke(c.io.archannel.valid, false)
+    poke(c.io.rchannel.ready, true)
+    // complete read hs
+    while (peek(c.io.rchannel.valid) == 0)
+      step(1)
+    expect(peek(c.io.rchannel.valid) == 1, "valid read")
+    expect(peek(c.io.rchannel.bits.rdata) == good, "data read")
+    // wait at least 1 cycle before deasserting the ready signal
+    step(1)
+    poke(c.io.rchannel.ready, false)
+    expect(peek(c.io.archannel.ready) == 1, "raddr channel ready")
+   }
+
+  println("Test start")
+  println("Test read")
+  // test read
+  read(0x00, 0x7eadbeef)
+  read(0x04, 0x0000beef)
+
+  step(1)
+  // test write
+  poke(c.io.awchannel.valid, true)
+  poke(c.io.awchannel.bits.awaddr, 0x04)
+  poke(c.io.awchannel.bits.awprot, 0x04)
+  while(peek(c.io.awchannel.ready) == 0)
+    step(1)
+  poke(c.io.wchannel.valid, true)
+  poke(c.io.wchannel.bits.wdata, 0x7fffffff)
+  while(peek(c.io.wchannel.ready) == 0)
+    step(1)
+  step(1) // ensure the wchannel valid stays for at least one clock
+  poke(c.io.wchannel.valid, false)
+  // check that the write response is ready
+  while(peek(c.io.bchannel.valid) == 0)
+    step(1)
+  expect(peek(c.io.bchannel.valid) == 1, "write response valid")
+  expect(peek(c.io.bchannel.bits.bresp) == 0x00, "write response value ok")
+  poke(c.io.bchannel.ready, true)
+  step(1)
+  expect(peek(c.io.bchannel.valid) == 0, "write response end")
+  // check the write
+  read(0x04, 0x7fffffff)
+
+  // while (peek(c.io.archannel.ready) == 0)
+  //   step(1)
+  println("Test end")
+
+}
+
+class IPBlock extends Module {
+
+  val s = Module(new AXI4LiteSlave())
+  val pa = Module(new Peripheral(0x7eadbeef))
+  val pb = Module(new Peripheral(0x0000beef))
+
+  val io = new Axi4LiteSlaveIf()
+
+  s.io.axi <> io
+
+  pa.io <> s.io.slaveA
+  pb.io <> s.io.slaveB
+
+}
+
+// object Testbench {
+
+//   def main(args: Array[String]): Unit = {
+//     val testArgs = args.slice(1, args.length)
+//     chiselMainTest(testArgs, () => Module(new AXI4LiteSlave
+// )) { c => new Axi4LiteTest(c)}
+//   }
+// }
+object Testbench {
+
+  def main(args: Array[String]): Unit = {
+    val testArgs = args.slice(1, args.length)
+    chiselMainTest(testArgs, () => Module(new IPBlock
+)) { c => new IPBlockTest(c)}
+  }
+}
